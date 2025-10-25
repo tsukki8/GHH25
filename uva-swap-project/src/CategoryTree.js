befe2e941e4e64e00cddad70e6a8a06da45614ca
tsukki8 <ashley.ashuo@gmail.com>
2025-10-25 18:36:06 -0400

    change categories to art-related

--- uva-swap-project/src/CategoryTree.js
+++ uva-swap-project/src/CategoryTree.js
@@ -7,23 +7,11 @@ function register(category) {
 }
 
 // roots - larger category
-const women = register(new Category(1, "Women"));
-const men = register(new Category(2, "Men"));
+const paintings = register(new Category(1, "Paintings"));
+const sculptures = register(new Category(2, "Sculptures"));
+const drawings = register(new Category(3, "Sculptures"));
+const digitalArt = register(new Category(4, "Digital Art"));
+const prints = register(new Category(5, "Prints"));
 
-// women subcategories- filtered under women
-women.addChild(register(new Category(11, "Dresses and Skirts")));
-women.addChild(register(new Category(12, "Tops")));
-women.addChild(register(new Category(13, "Pants")));
-women.addChild(register(new Category(14, "Coats")));
-women.addChild(register(new Category(15, "Shoes")));
-women.addChild(register(new Category(16, "Accessories")));
-
-// men subcategories- filtered under men
-men.addChild(register(new Category(21, "Tops")));
-men.addChild(register(new Category(22, "Bottoms")));
-men.addChild(register(new Category(23, "Shoes")));
-men.addChild(register(new Category(24, "Coats")));
-men.addChild(register(new Category(25, "Accessories")));
-
-// export root list (useful in UI later)
-const rootCategories = [women, men];
+// export root list (useful in UI later), ignore child subcategories for now
+const rootCategories = [paintings, sculptures, drawings, digitalArt, prints];

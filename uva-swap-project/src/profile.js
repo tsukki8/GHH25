cc64cbee179e52a30fc3a429955bfd713c1f4b07
Maggie Zhang <maggieshzh121@gmail.com>
2025-10-26 14:51:09 -0400

    Added a basic profile page

--- uva-swap-project/src/profile.js
+++ uva-swap-project/src/profile.js
@@ -1,3 +1,6 @@
+//import the user 
+import User from "./User.js";
+
 // Make header clickable to go back to search page
 document.addEventListener("DOMContentLoaded", () => {
     const homeTitle = document.getElementById("homeTitle");
@@ -6,6 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = "searchPage.html";
         });
     }
+
+    const profilePic = document.getElementById("profilePicture");
+    const profileName = document.getElementById("profileName");
+    const profileBio = document.getElementById("profileBio");
+
+    if (currentUser.pfp) profilePic.src = currentUser.pfp;
+    profileName.textContent = currentUser.username;
+    profileBio.textContent = currentUser.bio;
 
     // Optional: make profile icon clickable (if needed)
     const profileIcon = document.querySelector(".profile-icon");
